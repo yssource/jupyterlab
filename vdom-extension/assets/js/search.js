a9cb2c3b6ee1d9d269d9a0b454443d842e726793
Jason Grout <jgrout6@bloomberg.net>
2019-08-06 13:31:20 -0700

    Deploy to GitHub Pages

---
+++ vdom-extension/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"32":"Variable"},"rows":[{"id":0,"kind":32,"name":"MIME_TYPE","url":"globals.html#mime_type","classes":"tsd-kind-variable"}]};
